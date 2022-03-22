0d9eb71d4eba4c6edd614283055889c45d17c436
solczak <seboo0505@gmail.com>
2022-03-22 20:03:26 +0100

    initial commit

---
+++ src/main/java/pl/seba/bookindex/BookIndexApplication.java
@@ -0,0 +1,13 @@
+package pl.seba.bookindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookIndexApplication.class, args);
+	}
+
+}
